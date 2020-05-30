714d68c2748f56e79095180f9595fd15d5a213db
chic-caller-7520 <arbrown110@gmail.com>
2020-05-30 03:26:01 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -28,7 +28,7 @@ class InteractiveRecord
   end
 
   def col_names_for_insert
-    self.class.col_names.delete_if {|col| col == "id"}.join (" , ")
+    self.class.colomn_names.delete_if {|col| col == "id"}.join (" , ")
   end
 
   def values_for_insert
